a6c5a266a128540299bd0f37c711e1a224369d55
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-06-01 16:59:57 -0400

    Update for Relational change / RelationalF
    We don't make terms in some interpreter monad, we just make terms which
    can be used to build a RelationalF term

--- Data/Relational/RelationalMapping.hs
+++ Data/Relational/RelationalMapping.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : Data.Relational.RelationalMapping
-Description : Mapping to Relational types.
+Description : Mapping to and from Relational types.
 Copyright   : (c) Alexander Vieth, 2015
 Licence     : BSD3
 Maintainer  : aovieth@gmail.com
@@ -25,24 +25,15 @@ Portability : non-portable (GHC only)
 module Data.Relational.RelationalMapping (
 
     RelationalMapping(..)
+  , RelationalTable
 
   , toRow
   , fromRow
 
-  , makeSelect
-  , makeInsert
-  , makeUpdate
-  , makeDelete
-
-  , select
-  , insert
-  , delete
-  , update
-
-  , SelectConstraint
-  , InsertConstraint
-  , DeleteConstraint
-  , UpdateConstraint
+  , rmselect
+  , rminsert
+  , rmupdate
+  , rmdelete
 
   ) where
 
@@ -51,16 +42,15 @@ import GHC.Exts (Constraint)
 import Data.Bijection
 import Data.Proxy
 import Data.Relational
-import Data.Relational.Universe
-import Data.Relational.Interpreter
 
--- | Instances of this class can be pushed to / pulled from a relational
---   interpreter.
-class ( KnownSymbol (RelationalTableName d)
-      , IsSubset (RelationalSchema d) (RelationalSchema d)
-      , IsSubsetUnique (RelationalSchema d) (RelationalSchema d)
-      )
-      => RelationalMapping d
+-- | Instances of this class are associated with some relational table type.
+--   The @rowBijection@ allows us to read and write to a table of this type.
+class
+    ( KnownSymbol (RelationalTableName d)
+    , IsSubset (RelationalSchema d) (RelationalSchema d)
+    , IsSubsetUnique (RelationalSchema d) (RelationalSchema d)
+    )
+    => RelationalMapping d
   where
 
   type RelationalTableName d :: Symbol
@@ -68,10 +58,12 @@ class ( KnownSymbol (RelationalTableName d)
 
   relationalSchema :: Proxy d -> Schema (RelationalSchema d)
 
-  relationalTable :: Proxy d -> Table '(RelationalTableName d, RelationalSchema d)
-  relationalTable proxy = Table Proxy (relationalSchema proxy)
+  relationalTable :: Proxy d -> Table (RelationalTable d)
+  relationalTable proxy = table (relationalSchema proxy)
 
   rowBijection :: d :<->: Row (RelationalSchema d)
+
+type RelationalTable d = '(RelationalTableName d, RelationalSchema d)
 
 toRow :: RelationalMapping d => d -> Row (RelationalSchema d)
 toRow = biTo rowBijection
@@ -79,98 +71,29 @@ toRow = biTo rowBijection
 fromRow :: RelationalMapping d => Row (RelationalSchema d) -> d
 fromRow = biFrom rowBijection
 
-makeSelect
+rmselect
   :: forall d condition .
      ( RelationalMapping d
      , IsSubset (Concat condition) (RelationalSchema d)
      )
-   => Proxy d
-   -> Condition condition
-   -> Select '(RelationalTableName d, RelationalSchema d) (RelationalSchema d) condition
-makeSelect proxy condition =
+  => Proxy d
+  -> Condition condition
+  -> Select '(RelationalTableName d, RelationalSchema d) (RelationalSchema d) condition
+rmselect proxy condition =
     Select
       (relationalTable proxy)
       (fullProjection (relationalSchema proxy))
       condition
 
-type family SelectConstraint datatype interpreter condition :: Constraint where
-  SelectConstraint d i c = (
-      RelationalMapping d
-    , RelationalInterpreter i
-    , Functor (InterpreterMonad i)
-    , IsSubset (Concat c) (RelationalSchema d)
-    , Every (InUniverse (Universe i)) (Snds (RelationalSchema d))
-    , Every (InUniverse (Universe i)) (Snds (Concat c))
-    , InterpreterSelectConstraint i (RelationalSchema d) (RelationalSchema d) c
-    , ConvertToRow (Universe i) (RelationalSchema d)
-    )
-
-select
-  :: SelectConstraint d i conditioned
-  => Proxy i
-  -> Proxy d
-  -> Condition conditioned
-  -> (InterpreterMonad i) [Maybe d]
-select proxyI proxyD condition =
-    let selectTerm = makeSelect proxyD condition
-    in  (fmap . fmap . fmap) (biFrom rowBijection) (interpretSelect' proxyI selectTerm)
-    -- ^ three fmaps, for the monad, the list, and the maybe.
-
-makeInsert
+rminsert
   :: forall d .
      ( RelationalMapping d
      )
   => d
   -> Insert '(RelationalTableName d, RelationalSchema d)
-makeInsert d = Insert (relationalTable (Proxy :: Proxy d)) (biTo rowBijection d)
+rminsert d = Insert (relationalTable (Proxy :: Proxy d)) (biTo rowBijection d)
 
-type family InsertConstraint datatype interpreter :: Constraint where
-  InsertConstraint d i = (
-      RelationalMapping d
-    , RelationalInterpreter i
-    , InterpreterInsertConstraint i (RelationalSchema d)
-    , Every (InUniverse (Universe i)) (Snds (RelationalSchema d))
-    )
-
-insert
-  :: InsertConstraint d interpreter
-  => Proxy interpreter
-  -> d
-  -> (InterpreterMonad interpreter) ()
-insert proxyI d =
-    let insertTerm = makeInsert d
-    in  interpretInsert proxyI insertTerm
-
-makeDelete
-  :: forall d c .
-     ( RelationalMapping d
-     , IsSubset (Concat c) (RelationalSchema d)
-     )
-  => Proxy d
-  -> Condition c
-  -> Delete '(RelationalTableName d, RelationalSchema d) c
-makeDelete proxyD condition = Delete (relationalTable proxyD) (condition)
-
-type family DeleteConstraint datatype interpreter condition :: Constraint where
-  DeleteConstraint d i condition = (
-      RelationalMapping d
-    , RelationalInterpreter i
-    , Every (InUniverse (Universe i)) (Snds (Concat (condition)))
-    , InterpreterDeleteConstraint i (RelationalSchema d) (condition)
-    , IsSubset (Concat condition) (RelationalSchema d)
-    )
-
-delete
-  :: DeleteConstraint d interpreter c
-  => Proxy interpreter
-  -> Proxy d
-  -> Condition c
-  -> (InterpreterMonad interpreter) ()
-delete proxyI proxyD condition =
-    let deleteTerm = makeDelete proxyD condition
-    in  interpretDelete proxyI deleteTerm
-
-makeUpdate
+rmupdate
   :: forall d c .
      ( RelationalMapping d
      , IsSubset (Concat c) (RelationalSchema d)
@@ -178,7 +101,7 @@ makeUpdate
   => d
   -> Condition c
   -> Update '(RelationalTableName d, RelationalSchema d) (RelationalSchema d) c
-makeUpdate d condition =
+rmupdate d condition =
     Update
       (relationalTable proxyD)
       (fullProjection (relationalSchema proxyD))
@@ -188,22 +111,12 @@ makeUpdate d condition =
     proxyD :: Proxy d
     proxyD = Proxy
 
-type family UpdateConstraint datatype interpreter condition :: Constraint where
-  UpdateConstraint d i condition = (
-      RelationalMapping d
-    , RelationalInterpreter i
-    , Every (InUniverse (Universe i)) (Snds (RelationalSchema d))
-    , Every (InUniverse (Universe i)) (Snds (Concat (condition)))
-    , InterpreterUpdateConstraint i (RelationalSchema d) (RelationalSchema d) (condition)
-    , IsSubset (Concat condition) (RelationalSchema d)
-    )
-
-update
-  :: UpdateConstraint d interpreter c
-  => Proxy interpreter
-  -> d
+rmdelete
+  :: forall d c .
+     ( RelationalMapping d
+     , IsSubset (Concat c) (RelationalSchema d)
+     )
+  => Proxy d
   -> Condition c
-  -> (InterpreterMonad interpreter) ()
-update proxyI d condition =
-    let updateTerm = makeUpdate d condition
-    in  interpretUpdate proxyI updateTerm
+  -> Delete '(RelationalTableName d, RelationalSchema d) c
+rmdelete proxyD condition = Delete (relationalTable proxyD) (condition)
